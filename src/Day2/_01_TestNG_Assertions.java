9c6d59b797d59fe892461509023b07595e274e9d
hmk10 <halitmk@gmail.com>
2022-01-31 20:41:10 -0500

    TestNG | Assertion

--- src/Day2/_01_TestNG_Assertions.java
+++ src/Day2/_01_TestNG_Assertions.java
@@ -1,7 +1,10 @@
 package Day2;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Random;
 
 public class _01_TestNG_Assertions {
 
@@ -35,6 +38,87 @@ public class _01_TestNG_Assertions {
         String[] actual = {"twenty","Hello"};
 
         Assert.assertEqualsNoOrder(actual, expected);
+    }
+
+    @Test
+    public void assertSame() {
+        String[] expected = {"TechnoStudy"};
+        String[] actual = {"TechnoStudy"};
+
+        Assert.assertSame(actual, expected, "Actual and Expected should point to same object!");
+    }
+
+    @Test
+    public void assertSame2() {
+        String[] expected = {"Hello"};
+        String[] actual = expected;
+
+        Assert.assertSame(actual, expected, "Actual and Expected should point to same object!");
+    }
+
+    @Test
+    public void assertNotSame() {
+        String[] expected = {"TechnoStudy"};
+        String[] actual = {"TechnoStudy"};
+
+        Assert.assertNotSame(actual, expected, "Actual and Expected should not point to same object");
+    }
+
+    @Test
+    public void assertNotSame2() {
+        String[] expected = {"Hello"};
+        String[] actual = expected;
+
+        Assert.assertNotSame(actual, expected, "Actual and Expected should not point to same object");
+    }
+
+    @Test
+    public void assertNotNull() {
+        String nullString = null;
+
+        Assert.assertNotNull(nullString);
+    }
+
+    @Test
+    public void assertNotNull2() {
+        String notNullString = "TestNG";
+
+        Assert.assertNotNull(notNullString);
+    }
+
+    public boolean isDataAvailable() {
+        Random rnd = new Random();
+        return rnd.nextBoolean();
+    }
+
+    @Test
+    public void assertTrue() {
+        boolean actual = isDataAvailable();
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void assertFalse() {
+        boolean actual = isDataAvailable();
+
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void assertFail() {
+        if (!isDataAvailable()) {
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void assertFail2() {
+        try {
+            // try to find element
+        } catch (NoSuchElementException ex) {
+            Assert.fail("Test failed because Selenium cannot find element!");
+        }
     }
 
 }
