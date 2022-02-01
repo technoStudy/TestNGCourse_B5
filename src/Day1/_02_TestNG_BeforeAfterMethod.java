d161e48c6430bab643e5ebf71f5756bb437b5dad
hmk10 <halitmk@gmail.com>
2022-01-31 19:13:28 -0500

    TestNG | @BeforeMethod - @AfterMethod

--- src/Day1/_02_TestNG_BeforeAfterMethod.java
+++ src/Day1/_02_TestNG_BeforeAfterMethod.java
@@ -1,55 +1,37 @@
 package Day1;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class _02_TestNG_BeforeAfterMethod {
 
-    WebDriver driver;
-
     @BeforeMethod // will get executed before each @Test!!!
     public void beforeEachTest() {
 
-        System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
-
-        driver = new ChromeDriver();
-
-        driver.get("https://techno.study/");
+        System.out.println("Before each @Test");
 
     }
 
     @AfterMethod // will get executed after each @Test!!!
     public void afterEachTest() {
 
-        driver.quit();
+        System.out.println("After each @Test");
 
     }
 
     @Test
     public void test1() {
-        String expectedURL = "https://techno.study/";
 
-        String actualURL = driver.getCurrentUrl();
+        System.out.println("Test1");
 
-        Assert.assertEquals(actualURL, expectedURL);
     }
 
     @Test
     public void test2() {
-        String expectedURL = "https://techno.study/sdetcourse";
 
-        WebElement sdetCourseLink = driver.findElement(By.xpath("//a[text()='SDET course']"));
-        sdetCourseLink.click();
+        System.out.println("Test2");
 
-        String actualURL = driver.getCurrentUrl();
-
-        Assert.assertEquals(actualURL, expectedURL);
     }
 
 
