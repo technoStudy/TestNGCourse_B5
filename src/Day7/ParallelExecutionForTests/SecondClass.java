dfae621658133d364420f7c52432dc1dd668855a
hmk10 <halitmk@gmail.com>
2022-02-08 21:20:18 -0500

    TestNG | Parallel Testing (tests)

---
+++ src/Day7/ParallelExecutionForTests/SecondClass.java
@@ -0,0 +1,24 @@
+package Day7.ParallelExecutionForTests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SecondClass {
+
+    @Test
+    public void navigateFacebook() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Library/Selenium/chromedriver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.facebook.com/");
+
+        Thread.sleep(3000);
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("facebook"));
+
+        driver.quit();
+    }
+
+}
